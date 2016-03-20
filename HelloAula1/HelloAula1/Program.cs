3be2266f32472aeeba23c407cbb4f87aecbca74b
Elena.Nikolova <eliqwerty@abv.bg>
2016-03-20 22:27:20 +0200

    Създаване на програма HelloAula1

---
+++ HelloAula1/HelloAula1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloAula1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello Aula1!");
+			Console.ReadKey ();
+		}
+	}
+}
